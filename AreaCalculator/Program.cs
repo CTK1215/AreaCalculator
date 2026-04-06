6dcdf7c8e648f76095508879fd82c037220ad21e
CTK1215 <ctkelly1277@gmail.com>
2026-04-06 16:38:28 -0700

    add triangle area method

--- AreaCalculator/Program.cs
+++ AreaCalculator/Program.cs
@@ -8,9 +8,17 @@ class Program
     }
 
     public static double CircleArea(double radius)
-
+    
+    
     {
         //Formula for the area of a circle
         return Math.PI * radius * radius;
+    }
+
+    public static double TriangleArea(double baseLength, double height)
+
+    {
+        //Formula for area of Triangle
+        return baseLength * height / 2;
     }
 }
