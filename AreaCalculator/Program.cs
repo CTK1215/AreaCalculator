b4cddfd7b86b124c07328f5bcd8cb7c6e7b9a6a2
CTK1215 <ctkelly1277@gmail.com>
2026-04-06 23:53:32 -0700

    added comments

--- AreaCalculator/Program.cs
+++ AreaCalculator/Program.cs
@@ -6,22 +6,26 @@ class Program
 {
     static void Main(string[] args)
     {
+        //finding the area of a circle user input 
         Console.WriteLine("Please enter the radius of the circle:");
         double radius = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine($"The area of your circle is {CircleArea(radius)}");
         
+        //Finding the area of a triangle
         Console.WriteLine("Now onto the area of your triangle, what is the base length of your triangle?");
         double baseLength = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("What is the height of your triangle?");
         double height = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine($"The area of your triangle is {TriangleArea(baseLength, height)}");
         
+        //Finding the area of a rectangle
         Console.WriteLine("You also wanted to find out the area of a rectangle, can you tell me what the length of your rectangle please?");
         double length = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("Thank you, now tell me what the width or your rectangle is?");
         double width = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine($"The area of your rectangle is {RectangleArea(length, width)}");
         
+        //Finding the area of a square
         Console.WriteLine("Alright, now we will determine the area of your square, what is the measurement of one side of your square?");
         double side = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine($"The area of your square is {SquareArea(side)}");
